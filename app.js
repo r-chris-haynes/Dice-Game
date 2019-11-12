c1d27bca31e641ca3f3d821b202e210ed9c736ea
chris haynes <r.chris.haynes@gmail.com>
2019-11-12 12:10:21 -0500

    added dice

--- app.js
+++ app.js
@@ -9,8 +9,11 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, dice;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
+
+dice = Math.floor(Math.random() * 6) + 1;
+console.log(dice);
