78f967b3e56716c62a8922ee2e10dd5a9da16fca
chris haynes <r.chris.haynes@gmail.com>
2019-11-12 12:35:34 -0500

    displaying activePlayer roll in the dom

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ var scores, roundScore, activePlayer, dice;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 0;
+activePlayer = 1;
 
 dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
+
+document.querySelector('#current-' + activePlayer).textContent = dice; 
