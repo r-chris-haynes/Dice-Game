7d393866c2453238732123740ee44c077c7020c9
chris haynes <r.chris.haynes@gmail.com>
2019-11-17 13:59:54 -0500

    changing active class so panel changes with current player

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 GAME RULES:
 
 - The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- In each turn, a player rolls a dice as many times as he whishes. Each result gets added to their ROUND score
+- BUT, if the player rolls a 1, all their ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that their ROUND score gets added to their GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
@@ -38,6 +38,12 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     } else {
         activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
         roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.remove('active');
+        document.querySelector('.player-1-panel').classList.add('active');
     }
 
 })
